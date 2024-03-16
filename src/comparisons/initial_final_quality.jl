2d6f17fbafcc73b8cc7478ad0540514a86aee77e
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2024-03-16 21:14:54 +0100

    Fix initial-final loop

--- src/comparisons/initial_final_quality.jl
+++ src/comparisons/initial_final_quality.jl
@@ -36,30 +36,23 @@ Compare initial solution cost with final solution cost (G,S)
 - `instances::List`: list of instances to perform test
 - `directory_path::Str`: path to directory containing instances
 """
-function initial_final_quality(iterations, instances, directory_path)
-    methods = [local_greedy_search, local_steepest_search]
-    for method in methods
-        for instance in instances
-            println(method, instance)
-            filename = joinpath(directory_path, instance * ".tsp")
-            tsp = read_tsp_file(filename)
-            N = size(tsp.distance_matrix)[1]
-            costs_i = []
-            costs_f = []
-            for i in 1:iterations
-                initial_solution = generate_random_permutation(N)
-                initial_cost = evaluate_solution(initial_solution, tsp.distance_matrix)
-                final_solution = method(initial_solution, tsp.distance_matrix)
-                final_cost = final_solution.cost
-                push!(costs_i, initial_cost)
-                push!(costs_f, final_cost)
-            end
-            save_path = "output/initial_final_quality/$method/$instance"
-            df = DataFrame(initial=costs_i, final=costs_f)
-            CSV.write(joinpath(save_path * ".csv"), df)
-            plot_costs(df, save_path)
-        end
+function initial_final_quality(iterations, tsp, method, instance)
+    N = size(tsp.distance_matrix)[1]
+    costs_i = []
+    costs_f = []
+    for i in 1:iterations
+        initial_solution = generate_random_permutation(N)
+        initial_cost = evaluate_solution(initial_solution, tsp.distance_matrix)
+        final_solution = method(initial_solution, tsp.distance_matrix)
+        final_cost = final_solution.cost
+        push!(costs_i, initial_cost)
+        push!(costs_f, final_cost)
     end
+    save_path = "output/initial_final_quality/$method/$instance"
+    df = DataFrame(initial=costs_i, final=costs_f)
+    CSV.write(joinpath(save_path * ".csv"), df)
+    plot_costs(df, save_path)
+
 end
 
 
@@ -76,12 +69,12 @@ function assess_qualities(directory_path, instances, iterations)
             println(method, instance)
             filename = joinpath(directory_path, instance * ".tsp")
             tsp = read_tsp_file(filename)
-            initial_final_quality(iterations, instances, directory_path)
+            initial_final_quality(iterations, tsp, method, instance)
         end
     end
 end
 
 
 directory_path = "data/SEL_tsp"
-chosen_instances = ["berlin52", "ch150", "pr76", "tsp225"]
+chosen_instances = ["st70"]
 assess_qualities(directory_path, chosen_instances, 200)
